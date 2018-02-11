6faaba3541a672bddb4c8aa341cdbd2a58900fbc
eduardocollm <coll.eduardo@gmail.com>
2018-02-11 08:55:50 -0600

    test add file

---
+++ src/main/java/com/collesoft/messagerouter/service/CommandLineMessageRouterImpl.java
@@ -0,0 +1,8 @@
+package com.collesoft.messagerouter.service;
+
+public class CommandLineMessageRouterImpl implements MessageRouterService {
+    @Override
+    public String sendMessage(String message) throws Exception {
+        return null;
+    }
+}
